2048a65e3099ec36729ded6c0bca8d7707c8538a
proxuser <emexci@gmail.com>
2013-11-02 21:47:56 +0100

    add i9190 and build all script

---
+++ build_all.sh
@@ -0,0 +1,10 @@
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_eur_lte.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_eur_3g.sh
---
+++ build_eur_3g.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CARRIER="eur_3g"
+export EXEC_LOKI="N"
+./build_master.sh
